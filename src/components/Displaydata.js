6478e9ea15703cfa81c90e3a43e1440c12dabb06
Abhinit <krabhinit60@gmail.com>
2022-09-25 06:57:21 +0530

    validation done

--- src/components/Displaydata.js
+++ src/components/Displaydata.js
@@ -1,25 +1,68 @@
-import React from 'react'
+import React from "react";
+import {NavLink, Routes,Route} from "react-router-dom";
+import "./style.css";
 
-function Displaydata ({data}) {
+
+function Displaydata({ data }) {
   return (
+    <>
 
+<Routes>
+  <Route path="/"></Route>
+  <Route path="/img" element={
+    <p>img</p>
+    
+  }></Route>
+  <Route path="/name" element={
+    <p>name</p>
+    
+  }></Route>
+  <Route path="/email" element={
+    <p>Email</p>
+    
+  }></Route>
+  <Route path="/description" element={
+    <p>Description</p>
+    
+  }></Route>
+  <Route path="/name"></Route>
+  <Route path="/email"></Route>
+  <Route path="/description"></Route>
+  
+ 
+</Routes>
     <div className="main">
-        <div className="imgcls">
-        <img src={data.data.avatar} alt='Img'/> 
-
-        </div>
-        <div className="name">
-        <h3>{data.data.first_name}</h3>
-        <span>{data.data.email}</span>
-        </div>
-        <div className="fllowers">
-        <span>Description : </span><span className='fdata'> {data.support.text}</span>
-        </div>
-       
-
+      <div className="menu">
+        <NavLink to="/" className="nav_link">Home</NavLink>
+        <NavLink to="/img" className="nav_link">Image</NavLink>
+        <NavLink to="/name" className="nav_link">Name</NavLink>
+        <NavLink to="/email" className="nav_link">Email</NavLink>
+        <NavLink to="/description" className="nav_link">Description</NavLink>
+      </div>
+      <div className="img">
+        <img 
+        src={data.data.avatar}
+         alt="Img" />
+      </div>
+      <div className="name">
+        <h3>Name: 
+          {data.data.first_name}
+        </h3>
+        <span>Email:
+           {data.data.email}
+           </span>
+        <p>Description :
+        <span className="fdata"> 
+        {data.support.text}
+        </span>
+           </p>
+      </div>
+      {/* <div className="fllowers">
+      </div> */}
     </div>
 
-  )
+    </>
+  );
 }
 
 export default Displaydata;
--- src/components/Userdata.js
+++ src/components/Userdata.js
@@ -1,67 +1,105 @@
-import React,{ useState } from 'react';
-// import '../components/style.css';
-import Displaydata from './Displaydata';
+import React, { useState,useRef } from "react";
+import './style.css';
+import Displaydata from "./Displaydata";
+import { useEffect } from "react";
 
 const Userdata = () => {
+  const [userName, setUserName] = useState();
+  const [adata, setData] = useState({});
 
-    const [userName, setUserName] = useState();
-    const [adata, setData] = useState({});
-  
-    const onChangeHandler = (e) => {
-        if(e.target.value<13 )
-            setUserName(e.target.value);
-        else
-            window.alert("value can't be greater than 12")
-    };
-  
-    const onSubmitHandler = (e) => {
-      e.preventDefault();
-      fetch(`https://reqres.in/api/users/${userName}`)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          if (data) {
-            setData(data);
-            // console.log(data.data.email);
-            // console.log(data.data.first_name);
-            // console.log(data.data.last_name);
-            // {(userName>12) ? alert("value cant be greater than 12"):}
-          }
-        });
-    };
-  
+  const inputElement = useRef();
 
+// function isNumber(str)
+// {
+//   if(str.trim()==='')
+//   return false;
+//   return !isNaN(str);
+// }
+  const onChangeHandler = (e) => {
+    if (e.target.value < 13 && !isNaN(e.target.value) ) setUserName(e.target.value);
+    else window.alert("Pls Enter number only and less than 12");
+ 
+ 
+ 
 
 
     
+  };
 
+  useEffect(()=>
+  {
+    inputElement.current.focus();
+
+  },[])
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    inputElement.current.focus();
+    fetch(`https://reqres.in/api/users/${userName}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data) {
+          setData(data);
+          // console.log(data.data.email);
+          // console.log(data.data.first_name);
+          // console.log(data.data.last_name);
+          // {(userName>12) ? alert("value cant be greater than 12"):}
+        }
+      });
+  };
 
   return (
     <>
       <div className="container">
         <div className="heading">
-          <h1>Search Github User By One Click</h1>
+          <h1>Get User Data</h1>
         </div>
         <form onSubmit={onSubmitHandler}>
-          <input type="text" placeholder="search user...."
-          value={userName}
-          
-            
-        
-          onChange={onChangeHandler} />
+          <input
+          ref={inputElement} 
+          className="ipt"
+            type="text"
+            placeholder="Enter User ID...."
+            value={userName}
+            onChange={onChangeHandler}
+          />
           <button className="btn">Search</button>
         </form>
-        
-        
-        {/* <Displaydata data={adata}></Displaydata> */}
 
+        {/* <Displaydata data={adata} ></Displaydata> */}
       </div>
+{
+  <p>{adata.data}</p>
+}
 
       
-    </>
+      
 
-  )
-}
+      {/* <div className="card mb-3" style={{maxWidth: '540px'}}>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img
+              src={adata.data.avatar}
+              class="img-fluid rounded-start"
+              alt="Img"
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{adata.data.first_name}</h5>
+              <h5 className="card-title">{adata.data.email}</h5>
+              <p className="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div> */}
+    </>
+  );
+};
 
 export default Userdata;
